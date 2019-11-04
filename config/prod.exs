6514681063510d7242e059cbddf17548faefab68
mrfirki <mrfikri94@gmail.com>
2019-11-04 11:04:12 +0800

    add heroku config

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :chat_app, ChatAppWeb.Endpoint,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [scheme: "https", host: "/chat-app-phoenix.herokuapp.com", port: 443],
+  cache_static_manifest: "priv/static/cache_manifest.json",
 
 # Do not print debug messages in production
 config :logger, level: :info
